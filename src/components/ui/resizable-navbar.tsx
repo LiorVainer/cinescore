ea25dc222459a4fa14cbb3c151d5912ed404a91b
Lior Vainer <liorvainer@gmail.com>
2025-10-09 12:21:31 +0300

    feat: add custom fonts for improved typography in the application

--- src/components/ui/resizable-navbar.tsx
+++ src/components/ui/resizable-navbar.tsx
@@ -108,7 +108,7 @@ export const Navbar = ({
             // IMPORTANT: Change this to class of `fixed` if you want the navbar to be fixed
             className={cn(
                 position === 'fixed' ? 'fixed inset-x-0 top-0 z-40 w-full' : 'sticky inset-x-0 top-0 z-40 w-full',
-                !condensed && 'border-b backdrop-blur-md dark:bg-neutral-950/95',
+                !condensed && 'border-b backdrop-blur-md ',
                 className,
             )}
             initial={{y: 0, opacity: 1}}
