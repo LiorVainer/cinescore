a16fb438ea0607a0b7167e69151db4d7ac8f314e
Lior Vainer <liorvainer@gmail.com>
2025-10-21 10:21:55 +0300

    feat: update top rated section width, padding, and shadow for improved layout and visual effect

--- src/app/[locale]/(landing)/page.tsx
+++ src/app/[locale]/(landing)/page.tsx
@@ -31,7 +31,7 @@ export default async function LandingPage({params}: Props) {
             </section>
 
             {/* Notice: remove items-center to let width expand */}
-                <section className="relative w-[260vw] md:w-[140vw] bg-gradient-to-t border-t-6 border-white bg-primary px-6 md:px-12 py-12 md:py-16 rounded-t-full flex flex-col justify-center items-center shadow-[inset_0_-40px_60px_rgba(0,0,0,0.6)]">
+                <section className="relative w-[240vw] md:w-[140vw] border-t-6 border-white bg-primary px-6 md:px-12 py-16 rounded-t-full flex flex-col justify-center items-center shadow-[inset_0_20px_30px_rgba(0,0,0,0.6)]">
                     <TopRatedCarousel movies={topMovies} />
                 </section>
         </main>
